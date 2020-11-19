b034a0183387b12167883fcbad7bc6a784b3dbed
Dmitry Slesarev <62387664+DmitrySlesarev@users.noreply.github.com>
2020-11-19 13:11:14 +0300

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -26,4 +26,47 @@ def drawBoard(board):
 	# Reflect the numbers in the upper part of play field.
 	print(tensDigitsLine)
 	print(' ' + ('0123456789' * 6))
-	print()	
+	print()
+
+	# Display each of 15 rows.
+	for row in range(15):
+	# A space should be added to each single-character number.
+		if row < 10:
+			extraSpace = " "
+		else:
+			extraSpace = " "
+
+		# Create string for this row on play field.
+		boardRow = " "
+		for column in range(60):
+			boardRow +=board[column][row]
+
+print('%s%s %s %s' % (extraSpace, row, boardRow, row))
+
+	# Display numbers in the in the bottom.
+	print()
+	print(" " + ("012345678" * 6))
+	print(tensDigitsLine)
+
+def getRandomChests(numChests):
+	# Create list of data structures.
+		chests = []
+		while len(chests) < numChests:
+			newChest = [random.randint(0, 59), random.randint(0, 14)]
+	if newChest not in chests: # Make sure, that the chest is not here.
+			chests.append(newChest)
+		return chests
+
+	def isOnBoard(x, y):
+		# Returns True, if x and y are present on the field. Otherwise, False.
+			return x >= 0 and x <= 59 and y >= 0 and y <=14 
+
+		def makeMove(board, chests, x, y):
+			# Change data structure, using sonar symbol. Delete chests,
+			# as soon as you got them. Return False, if the move is
+			# not possible. Otherwise, return string with result.
+			smallestDistance = 100 # All the chest will be placed nearer, than 100 items.
+			for cx, cy in chests:
+				distance = math.sqrt((cx-x)*(cx-x) + (cy-y)*(cy-y))
+
+					
